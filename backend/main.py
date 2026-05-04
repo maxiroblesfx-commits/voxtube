0429fb95bc545ace8d37c5fe08daf42b4b19f240
MaxiRobles <you@example.com>
2026-05-04 04:58:24 -0300

    Allow all origins in CORS

--- backend/main.py
+++ backend/main.py
@@ -38,8 +38,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
